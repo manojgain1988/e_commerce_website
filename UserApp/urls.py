1817acc72715a969ea4fcc6b0b9d13ac0a73fbb2
manojgain1988 <manojgain1988@gmail.com>
2024-05-08 22:50:13 +0600

    somethong added

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-
+from UserApp.views import user_logout
 urlpatterns = [
-    # path('', include('ecomapp.urls')),
+    path('logout/', user_logout,name='user_logout'),
   
    
 ]
